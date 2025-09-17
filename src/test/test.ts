49421e37f7853e2c4c9da979283796c18518e31e
Boinga15 <realboinga@gmail.com>
2025-09-17 10:06:22 +0100

    Updated and fixed test.

--- src/test/test.ts
+++ src/test/test.ts
@@ -129,11 +129,11 @@ game.level!.addWidget(new TestWidget2(game));
 game.level!.addWidget(new TestWidget2(game));
 
 assert(game.level!.widgets.length == 3, "Assertion Failure - " + game.level!.widgets.length.toString() + " widgets found in level instead of three.");
-assert(game.level!.getAllWidgetsOfClass(TestWidget2).length == 2, "Assertion Failure - " + game.level!.getAllWidgetsOfClass(TestWidget2).length.toString() + " instances of TestWidget2 found instead of two instances.");
+assert(game.level!.getWidgetsOfClass(TestWidget2).length == 2, "Assertion Failure - " + game.level!.getWidgetsOfClass(TestWidget2).length.toString() + " instances of TestWidget2 found instead of two instances.");
 
-game.level!.removeWidget(game.level!.getAllWidgetsOfClass(TestWidget1)[0]);
+game.level!.removeWidget(game.level!.getWidgetsOfClass(TestWidget1)[0]);
 assert(game.level!.widgets.length == 2, "Assertion Failure - " + game.level!.widgets.length.toString() + " widgets found in level instead of two after deletion.");
-assert(game.level!.getAllWidgetsOfClass(TestWidget2).length == 2, "Assertion Failure - " + game.level!.getAllWidgetsOfClass(TestWidget2).length.toString() + " instances of TestWidget2 found instead of two instances after deletion.");
+assert(game.level!.getWidgetsOfClass(TestWidget2).length == 2, "Assertion Failure - " + game.level!.getWidgetsOfClass(TestWidget2).length.toString() + " instances of TestWidget2 found instead of two instances after deletion.");
 
 const mainWidget = new TestWidget3(game);
 const subWidget = new TestWidget1(game);
@@ -145,26 +145,26 @@ game.level!.addWidget(mainWidget);
 
 assert(game.level!.getWidgetOfClass(TestWidget3) === mainWidget, "Assertion Failure - getWidgetOfClass for level returns wrong result.");
 assert(mainWidget.subWidgets.length == 3, "Assertion Failure - " + mainWidget.subWidgets.length.toString() + " sub-widgets found instead of three.");
-assert(mainWidget.getAllSubWidgetsOfClass(TestWidget2).length == 1, "Assertion Failure - " + mainWidget.getAllSubWidgetsOfClass(TestWidget2).length.toString() + " instances of TestWidget2 found as subWidgets instead of one.");
+assert(mainWidget.getSubWidgetsOfClass(TestWidget2).length == 1, "Assertion Failure - " + mainWidget.getSubWidgetsOfClass(TestWidget2).length.toString() + " instances of TestWidget2 found as subWidgets instead of one.");
 assert(mainWidget.getSubWidgetOfClass(TestWidget1) === subWidget, "Assertion Failure - getWidgetOfClass for widget returns wrong result.");
 
 mainWidget.removeSubWidget(subWidget);
 
 assert(mainWidget.subWidgets.length == 2, "Assertion Failure - " + mainWidget.subWidgets.length.toString() + " sub-widgets found instead of two after deletion.");
-assert(mainWidget.getAllSubWidgetsOfClass(TestWidget2).length == 1, "Assertion Failure - " + mainWidget.getAllSubWidgetsOfClass(TestWidget2).length.toString() + " instances of TestWidget2 found as subWidgets instead of one.");
+assert(mainWidget.getSubWidgetsOfClass(TestWidget2).length == 1, "Assertion Failure - " + mainWidget.getSubWidgetsOfClass(TestWidget2).length.toString() + " instances of TestWidget2 found as subWidgets instead of one.");
 
 game.addPersistantWidget(new TestWidget1(game));
 game.addPersistantWidget(new TestWidget1(game));
 game.addPersistantWidget(new TestWidget3(game));
 
 assert(game.persistantWidgets.length == 3, "Assertion Failure - " + game.persistantWidgets.length.toString() + " persistant widgets found instead of three.");
-assert(game.getAllPersistantWidgetsOfClass(TestWidget1).length == 2, "Assertion Failure - " + game.getAllPersistantWidgetsOfClass(TestWidget1).length.toString() + " instances of TestWidget1 found as persistant widgets instead of two.");
-assert(game.getPersistantWidgetOfClass(TestWidget3) === game.getAllPersistantWidgetsOfClass(TestWidget3)[0], "Assertion Failure - getPersistantWidgetOfClass returns incorrect result.")
+assert(game.getPersistantWidgetsOfClass(TestWidget1).length == 2, "Assertion Failure - " + game.getPersistantWidgetsOfClass(TestWidget1).length.toString() + " instances of TestWidget1 found as persistant widgets instead of two.");
+assert(game.getPersistantWidgetOfClass(TestWidget3) === game.getPersistantWidgetsOfClass(TestWidget3)[0], "Assertion Failure - getPersistantWidgetOfClass returns incorrect result.")
 
 game.removePersistantWidget(game.getPersistantWidgetOfClass(TestWidget1)!);
 
 assert(game.persistantWidgets.length == 2, "Assertion Failure - " + game.persistantWidgets.length.toString() + " persistant widgets found instead of two after deletion.");
-assert(game.getAllPersistantWidgetsOfClass(TestWidget1).length == 1, "Assertion Failure - " + game.getAllPersistantWidgetsOfClass(TestWidget1).length.toString() + " instances of TestWidget1 found as persistant widgets instead of one after deletion.");
+assert(game.getPersistantWidgetsOfClass(TestWidget1).length == 1, "Assertion Failure - " + game.getPersistantWidgetsOfClass(TestWidget1).length.toString() + " instances of TestWidget1 found as persistant widgets instead of one after deletion.");
 
 game.loadLevel(new Level1(game));
 
